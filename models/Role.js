644df524cb1211154ea578a70b32cc089a79702b
vansharora03 <vansh1993@icloud.com>
2023-04-02 22:52:12 -0400

    Added role model

---
+++ models/Role.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+// Specify a role schema
+const RoleScehma = new mongoose.Schema({
+    name: String
+});
+
+// Utilize schema to export model
+module.exports = mongoose.model('Role', RoleScehma);
