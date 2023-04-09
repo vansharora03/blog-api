17bb038f554996dcbb5596011dce691c09ed1553
vansharora03 <vansh1993@icloud.com>
2023-04-08 21:07:49 -0400

    Added first name and last name to User model

--- models/User.js
+++ models/User.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 // Specify a user schema
 const UserSchema = new mongoose.Schema({
     username: String,
+    first_name: String,
+    last_name: String,
     password: String,
     email: String,
     role: [{type: mongoose.Schema.Types.ObjectId, ref: 'Role'}]
