c5faf57d9bf852d240de13b412955aa7d3b38b68
vansharora03 <vansh1993@icloud.com>
2023-04-10 00:57:58 -0400

    Added passport LocalStrategy support

--- app.js
+++ app.js
@@ -1,21 +1,55 @@
+// Import packages and dependencies
 var createError = require('http-errors');
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var path = require('path');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const User = require('./models/User');
 require('dotenv').config();
 
 // Connect to mongodb
 mongoose.connect(process.env.MONGODB);
 
+// Require routes
 var indexRouter = require('./routes/index');
 const blogsRouter = require('./routes/blogs');
 const usersRouter = require('./routes/users'); 
 const signupRouter = require('./routes/signup');
+const loginRouter = require('./routes/login');
 
+// Use express
 var app = express();
 
+// Passport configuration
+passport.use(new LocalStrategy(async (username, password, done) => {
+  try {
+    const user = await User.findOne({ username });
+    
+    // invalid username
+    if (!user) {
+      return done(null, false, {message: 'Invalid username or password'});
+    }
+
+    // check password
+    bcrypt.compare(password, user.password, (err, res) => {
+      if (res) {
+        // passwords match, log in
+        return done(null, user)
+      } else {
+        return done(null, false, {message: 'Invalid username or password'});
+      }
+    });
+
+  } catch (err) {
+    return done(err);
+  }
+}))
+
+app.use(passport.initialize());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,6 +60,7 @@ app.use('/', indexRouter);
 app.use('/blogs', blogsRouter);
 app.use('/users', usersRouter);
 app.use('/signup', signupRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
